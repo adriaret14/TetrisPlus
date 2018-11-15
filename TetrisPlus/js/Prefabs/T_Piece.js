b43e2d073e08344b283ef255407bb5e837b9e66b
Adria Fret <adriafret1995@gmail.com>
2018-11-15 13:36:40 +0100

    More prefabs

--- TetrisPlus/js/Prefabs/T_Piece.js
+++ TetrisPlus/js/Prefabs/T_Piece.js
@@ -1,19 +1,29 @@
 var Phaser = Phaser || {};
 var tetrisPlus = tetrisPlus || {};
 
-tetrisPlus.T_Piece = function(game, x, y)
+tetrisPlus.T_Piece = function(game, x1, y1)
 {
-    Phaser.Sprite.call(this,game,x,y,'T');
-    
-    this.x=x;
-    this.y=y;
-    //this.anchor.setTo(.5);
+    //Phaser.Sprite.call(this,game,x1,y1,'T');
+    //Phaser.Sprite.call(this,game,x2,y2,'T');
+    Phaser.Sprite.call(this,game,x1,y1,'T_Complete');
     this.scale.setTo(2);
+    //this.anchor.setTo(0.5, 0.25);
+    
+    this.x1=x1;
+    this.y1=y1;
+    //this.anchor.setTo(.5);
+    
+    //this.cursors = this.gameState.game.input.keyboard.createCursorKeys();
+    
 };
 tetrisPlus.T_Piece.prototype = Object.create(Phaser.Sprite.prototype);
 tetrisPlus.T_Piece.prototype.constructor = tetrisPlus.T_Piece;
 tetrisPlus.T_Piece.prototype.move=function(direction, distance)
 {
+     /*if (this.cursors.right.isDown)
+         {
+             console.log("HOLA");
+         }*/
     //console.log("HOLA");
     if(direction==1)
         {
@@ -23,5 +33,21 @@ tetrisPlus.T_Piece.prototype.move=function(direction, distance)
         {
             this.x+=distance;
         }
+    else if(direction==3)
+        {
+            this.y+=distance;
+        }
 };
+tetrisPlus.T_Piece.prototype.drop=function()
+{
+    //console.log("HOLA");
+
+};
+tetrisPlus.T_Piece.prototype.update=function()
+{
+    //console.log("HOLA");
+    
+
+};
+
 
