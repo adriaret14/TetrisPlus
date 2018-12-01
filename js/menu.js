b5c752de3fde6f974be5cccae1fc8c15937235f3
Unknown <ignacioperezvelasco12@gmail.com>
2018-12-01 18:37:50 +0100

    STarting Menu uploaded img of buttons and trying

--- js/menu.js
+++ js/menu.js
@@ -1,24 +1,43 @@
 var Phaser = Phaser || {};
 var tetrisPlus = tetrisPlus || {};
 
+//Button variables
+var buttonPlay;
+var buttonPlayPuzzle1;
+var buttonPlayPuzzle2;
+var buttonPlayPuzzle3;
+var buttonPlayClassic;
+var buttonExit;
+
+//Background
+var background;
+
+
 tetrisPlus.menuState = {
     
     init:function(){
-        
+         this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     },
     
     preload:function(){
-        
+        this.game.load.image('backgroundMenu', 'assets/img/backgroundMm.png');   
+        this.game.load.image('buttonClassicImg','assets/img/buttonClassic.png',200,180);
     },
     
-    create:function(){        
-
+    create:function(){   
+        this.background = this.game.add.image(0,0,'backgroundMenu');        
+        buttonPlayClassic = this.game.add.button(0, 0, 'buttonClassicImg', actionOnClick, 0, 2, 1, 0);
     },
     
     update:function(){
         
     },
+    actionOnClick:function()
+    {
+    
+    },
 };
+
 
  
 
